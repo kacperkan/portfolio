58da159018d9d9da0697f5bb67c38effca5c403a
Kacper Kania <kacp.kania@gmail.com>
2020-01-03 22:27:46 +0100

    Updates

---
+++ precache-manifest.9a7e0b664640bcf7bb4ef6fb5a1980ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99a951921e7a1f85d96c4525a7c867c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c4699db7aa79fbe055",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9744edfe478b015a84d2",
+    "url": "/static/css/main.a23255e5.chunk.css"
+  },
+  {
+    "revision": "d4c4699db7aa79fbe055",
+    "url": "/static/js/2.d3e5f7c6.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.d3e5f7c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9744edfe478b015a84d2",
+    "url": "/static/js/main.5d19f1e8.chunk.js"
+  },
+  {
+    "revision": "bc0a437ac56b6d303c33",
+    "url": "/static/js/runtime-main.ca2d27e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3cd22121dddb8340d3d3579f9011f32.js"
+  "/precache-manifest.9a7e0b664640bcf7bb4ef6fb5a1980ba.js"
 );
 
 self.addEventListener('message', (event) => {
