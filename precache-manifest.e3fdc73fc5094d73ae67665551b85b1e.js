c2d8e7685370ef1932d1991362bbf9b73d991954
Kacper Kania <kacp.kania@gmail.com>
2020-01-03 11:53:10 +0100

    Updates

---
+++ precache-manifest.e3fdc73fc5094d73ae67665551b85b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c440ffd8783df5fa88d9432712552d7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76926405d84c3fe1c766",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ea94a6515a0d2d29785",
+    "url": "/portfolio/static/js/2.73aba01a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.73aba01a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76926405d84c3fe1c766",
+    "url": "/portfolio/static/js/main.c7e1edaa.chunk.js"
+  },
+  {
+    "revision": "f5ff1fe24da837620bb5",
+    "url": "/portfolio/static/js/runtime-main.c507fcf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
